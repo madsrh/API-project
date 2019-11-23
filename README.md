c3b758aaf570f449699e3b895d9d1624ca8b36e3
madsrh <madsrh@gmail.com>
2019-11-23 21:34:41 +0100

    Added courses
    Finally succesed with the courses API. Just change the CouseId as you please.

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Get API info here: https://api.speedadmin.dk/Help
 
 - [x] News page
 - [x] Teacher list page
-- [ ] Courses pages - displays a page about eg. guitar lessons (Courses API)
+- [x] Courses pages - displays a page about eg. guitar lessons (Courses API)
 - [ ] Concert list - lists the upcoming concerts (Booking API)
---
+++ courses.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+
+<?php
+    function executeRESTCall($method, $url) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+
+        $header = array();
+        $header[] = 'Content-type: application/json';
+        $header[] = 'Authorization: 1ThIsIsRanDomLettERsCkQKkjiOi2pZwA1shPrandomStUfF';
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        return curl_exec($curl);
+    }
+    $baseUrl = 'https://api.speedadmin.dk/v1/';
+
+    $coursesJSONString = executeRESTCall('GET', $baseUrl . 'courses');
+
+    // Convert JSON string to Object
+    $courses = json_decode($coursesJSONString);
+
+    // Loop through Object
+    foreach($courses as $key => $value) {
+         // Finds courseid 195
+        if  ($value->CouseId == "195") {
+       echo '<h1>' . $value->Text . '</h1>';
+       echo 'Php script array key: ' . $key . '<br>';
+       echo $value->Description;
+       echo '<h3>Subject:</h3>';
+       echo $value->Subject;
+       echo '<h3>Categori:</h3>';
+       echo $value->Categori;
+       echo '<h3>CouseId:</h3>';
+       echo $value->CouseId;
+       echo '<h3>Course:</h3>';
+       echo $value->Course;
+       echo '<h3>OnWaitingList:</h3>';
+       echo $value->OnWaitingList;
+       echo '<h3>CategoriId / TreeId:</h3>';
+       echo $value->CategoriId;
+       echo '<h3>SubjectCodeId:</h3>';
+       echo $value->SubjectCodeId;
+
+        }
+    }
+
+?>
+
+</html>
