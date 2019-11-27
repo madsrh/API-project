9ea1fd2aea06177103fe73e509080b020cc259f7
MadsRH <madsrh@gmail.com>
2019-11-28 00:00:48 +0100

    Adding placeholder images
    Adding placeholder images for teachers with no image using getimagesize

--- teachers.php
+++ teachers.php
@@ -1,4 +1,4 @@
-<table>
+<table width="100%">
 <?php
     function executeRESTCall($method, $url) {
         $curl = curl_init();
@@ -15,32 +15,43 @@
     }
     $baseUrl = 'https://api.speedadmin.dk/v1/';
 
-        $teachersJSONString = executeRESTCall('GET', $baseUrl . 'teachers');
-        // $getMads = executeRESTCall('GET', 'https://api.speedadmin.dk/v1/teachers/496');
+         $teachersJSONString = executeRESTCall('GET', $baseUrl . 'teachers');
+            // $getMads = executeRESTCall('GET', 'https://api.speedadmin.dk/v1/teachers/496');
 
 
-        // Convert JSON string to Object
-        $teachers = json_decode($teachersJSONString);
-        // Printing all teachers, then you can see what values you have in there
-        // print_r($teachers);
+            // Convert JSON string to Object
+            $teachers = json_decode($teachersJSONString);
 
-        // Loop through Object
-        foreach($teachers as $key => $value) {
+
+            // Use print_r($teachers); to printing all teachers, then you can see what values you have in there
+
+
+            // Loop through Object
+            foreach($teachers as $key => $value) {
+
+
+              $external_link = "https://www.madsrosendahl.dk/a/img/Teacher_". strval($value->TeacherId) . ".jpg";
+            // define a variable for the image url with the teacher ID
             if (true) {
-                      echo '<tr><td>';
-                      echo '<img src="https://www.jammerbugtkulturskole.dk/Files/Cache.net/SpeedAdmin/Teacher_'. strval($value->TeacherId) .'.jpg" width="75px" height="98px"/></td>';
-                      echo '<td><strong>' . ($value->Name . "\n\n") . ($value->Surname) . '</strong>';
-                      echo '<ul>';
+                echo '<tr><td>';
+                      if (@getimagesize($external_link)) {
+                        echo '<img src="' . $external_link . '" width="75px" height="98px"/></td>';
+                      } else {
+                        echo '<img src="https://www.madsrosendahl.dk/a/img/Teacher_missing.jpg" width="75px" height="98px"/></td>';
+                        // url to placeholder image if there's no image available with teacher ID
+                      }
+                          echo '<td><strong>' . ($value->Name . "\n\n") . ($value->Surname) . '</strong>';
+                          echo '<ul>';
 
-                      // There might be a better way to do this, but it works :)
-                $listemedfag = $value->AvailableClasses;
-                      for ($i = 0; $i < count($listemedfag); $i++)
-                        echo '<li>' . $listemedfag[$i] . '</li> ' . "\n";
-                        echo '</ul>';
+                          // There might be a better way to do this, but this works :)
+                          $listemedfag = $value->AvailableClasses;
+                          for ($i = 0; $i < count($listemedfag); $i++)
+                            echo '<li>' . $listemedfag[$i] . '</li> ' . "\n";
+                            echo '</ul>';
 
-    						echo '<td>Email:' . ' ' . ($value->Email . "\n\n");
-                echo '<br>Tlf. ' . ' ' .($value->Mobile . "\n\n");
-                echo '</td></tr>';
+        						echo '<td>Email:' . ' ' . ($value->Email . "\n\n");
+                    echo '<br>Tlf. ' . ' ' .($value->Mobile . "\n\n");
+                    echo '</td></tr>';
 
             }
         }
