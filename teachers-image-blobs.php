a971b6cc62dc4a10fa57a979f5032e69315bc0a4
Paul Kepinski <paul.kepinski@orange.fr>
2022-01-27 11:07:25 +0100

    Use get-blob.php for teachers blob

--- teachers-image-blobs.php
+++ teachers-image-blobs.php
@@ -1,21 +1,16 @@
-
 <?php
 
-  require_once(__DIR__.'/lib/executeRESTCall.php');
+require_once(__DIR__.'/lib/executeRESTCall.php');
 
-          // executeRESTCall('GET', 'https://api.speedadmin.dk/v1/teachers/496');
-            $teachersJSONString = executeRESTCall('GET', '/teachers');
+// executeRESTCall('GET', 'https://api.speedadmin.dk/v1/teachers/496');
+$teachersJSONString = executeRESTCall('GET', '/teachers');
 
-            // Convert JSON string to Object and returns an array
-            $teachers = json_decode($teachersJSONString);
+// Convert JSON string to Object and returns an array
+$teachers = json_decode($teachersJSONString);
 
-            // Use a foreach loop to grab the each individual teachers blob
-            foreach($teachers as $key => $value) {
-              // gets the raw image data
-              $blobJSONString = executeRESTCall('GET', '/blobs/' . $value->Blob->BlobId);
-
-              // saviour line! This turns the image data into an image
-              echo "<img src='data:image/jpeg;base64," . base64_encode( $blobJSONString )."' width='80px' width='120px'><br>";
-        }
-
-        ?>
+// Use a foreach loop to grab the each individual teachers
+foreach($teachers as $teacher) {
+  if (null !== $teacher->Blob->BlobId) {
+    echo sprintf("<img src='./get-blob.php?id=%s' height='120px' loading='lazy'><br>", $teacher->Blob->BlobId);
+  }
+}
